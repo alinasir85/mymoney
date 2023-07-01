5a2852411c2bbb7cb05472073d9a6ee1e3d83816
Ali Nasir <alinasir85@live.com>
2023-07-01 21:34:34 +0500

    deploy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/mymoney',
-  assetPrefix: '/mymoney/'
+  assetPrefix: '/mymoney/',
+  generateBuildId: async () => {
+    return 'nextjs-build';
+  },
+  generateEtags: false
 }
 
 module.exports = nextConfig
